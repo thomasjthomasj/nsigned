aca459d0483feaa211af8dcd53a7811de07f9be9
thomasjthomasj <thomas.marchant@justabout.com>
2026-04-22 15:13:33 +0100

    install nextapp

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // allowedDevOrigins: [
+  //   "http://localhost:9000",
+  //   "http://localhost:3000",
+  // ]
+};
+
+export default nextConfig;
