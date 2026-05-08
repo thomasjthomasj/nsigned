139b57dd0d8c3290211e446b668a1eb9a499cdca
thomasjthomasj <thomas.marchant@justabout.com>
2026-05-08 11:29:31 +0100

    remove frontend docker image

--- apps/frontend/app/_utils/api.ts
+++ apps/frontend/app/_utils/api.ts
@@ -1,9 +1,6 @@
-const CLIENT_API_URL = process.env.NEXT_PUBLIC_API_URL;
-const SERVER_API_URL = process.env.SSR_API_URL;
-
 import type { QueryParams } from "@/_types";
 
-const API_URL = typeof window === "undefined" ? SERVER_API_URL : CLIENT_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const getEndpoint = (endpoint: string) => `${API_URL}/${endpoint}`;
 
 export const getQueryString = (data: QueryParams = {}) => {
--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+
 };
 
 export default nextConfig;
