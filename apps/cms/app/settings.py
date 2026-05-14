9e86c25be564def0ade54597496e2f4bfecadc50
thomasjthomasj <thomas.marchant@justabout.com>
2026-05-14 15:55:57 +0100

    setup whitenoise

--- apps/cms/app/settings.py
+++ apps/cms/app/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'users.middleware.AuthMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,4 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
