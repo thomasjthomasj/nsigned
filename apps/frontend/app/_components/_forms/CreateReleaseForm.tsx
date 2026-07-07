b0daddf84d5416630ff76deb88cae655e5291249
thomasjthomasj <thomas.marchant@justabout.com>
2026-07-07 11:23:19 +0100

    stub out create release page

---
+++ apps/frontend/app/_components/_forms/CreateReleaseForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { Button } from "@/_components/Button";
+import { FormField } from "@/_components/FormField";
+import { useAuth } from "@/_hooks";
+import { post } from "@/_utils/api.client";
+
+export const CreateReleaseForm = () => {
+
+  return null;
+};
