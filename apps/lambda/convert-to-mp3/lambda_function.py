174b6719943f3d82c3826114a012a10026d813db
thomasjthomasj <thomas.marchant@justabout.com>
2026-07-09 11:50:32 +0100

    update lambda

--- apps/lambda/convert-to-mp3/lambda_function.py
+++ apps/lambda/convert-to-mp3/lambda_function.py
@@ -6,23 +6,30 @@ from urllib.parse import unquote_plus
 s3 = boto3.client("s3")
 bucket = "nsigned"
 
+raw_path = "audio/raw/"
+mp3_path = "audio/mp3s/"
+
 def lambda_handler(event, context):
   record = event["Records"][0]
   key = unquote_plus(record["s3"]["object"]["key"])
   print(f"KEY: {key}")
 
-  if not key.startswith("audio/raw/"):
+  if not key.startswith(raw_path):
     print("NOT ADDED TO audio/raw/")
     return {
       "statusCode": 200,
       "message": "File not converted - not added to audio/raw/",
     }
 
+  relative_path = key[len(raw_path)]
+  track_id = os.path.splitext(relative_path)[0]
   filename = os.path.basename(key)
-  track_id = os.path.splitext(filename)[0]
 
   tmp_input = f"/tmp/{filename}"
   tmp_mp3 = f"/tmp/{track_id}.mp3"
+
+  mp3_key = f"{mp3_path}{track_id}.mp3"
+  print(f"MP3 KEY: {mp3_key}")
 
   s3.download_file(bucket, key, tmp_input)
 
@@ -36,9 +43,6 @@ def lambda_handler(event, context):
     "320k",
     tmp_mp3,
   ], check=True)
-
-  mp3_key = f"audio/mp3s/{track_id}.mp3"
-  print(f"MP3 KEY: {mp3_key}")
 
   s3.upload_file(tmp_mp3, bucket, mp3_key)
   print("SUCCESSFULLY CONVERTED")
