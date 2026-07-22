c5bc077e9030b3d6786f681f8296b8ca3472cebf
thomasjthomasj <thomas.marchant@justabout.com>
2026-07-22 14:56:52 +0100

    endpoint for getting image upload urlk

--- apps/cms/app/s3.py
+++ apps/cms/app/s3.py
@@ -7,3 +7,10 @@ s3_audio = boto3.client(
   aws_secret_access_key=settings.AWS_S3_AUDIO_ACCESS_SECRET,
   region_name=settings.AWS_S3_REGION,
 )
+
+s3_images = boto3.client(
+  "s3",
+  aws_access_key_id=settings.AWS_S3_AUDIO_ACCESS_KEY,
+  aws_secret_access_key=settings.AWS_S3_AUDIO_ACCESS_SECRET,
+  region_name=settings.AWS_S3_REGION,
+)
--- apps/cms/images/views.py
+++ apps/cms/images/views.py
@@ -1,8 +1,44 @@
+from django.db import transaction
 from app.decorators import method, logged_in
-from app.http import Ok
+from app.http import Ok, BadRequest
+from app.s3 import s3_images
+from app.utils import slugify
 from .models import ImageUpload
 
 @method("POST")
 @logged_in()
+@transaction.atomic()
 def start_upload(request):
-  return Ok()
+  data = request.json
+  user = request.site_user
+  filename = data.get("filename")
+  filetype = data.get("filetype")
+  if filetype not in ["jpg", "png"]:
+    return BadRequest("Image must be either .jpg or .png file")
+  base_location = f"images/{slugify(user.username)}/{slugify(filename)}"
+  lg_location = f"{base_location}_lg.{filetype}"
+  md_location = f"{base_location}_md.{filetype}"
+  sm_location = f"{base_location}_sm.{filetype}"
+
+  image_upload = ImageUpload.objects.create(
+    created_by=user,
+    filetype=filetype,
+    lg_location=lg_location,
+    md_location=md_location,
+    sm_location=sm_location,
+  )
+
+  presigned_url = s3_images.generate_presigned_url(
+    "put_object",
+    Params={
+      "Bucket": "nsigned",
+      "Key": lg_location,
+      "ContentType": "image/jpeg" if filetype == "jpg" else "image/png",
+    },
+    ExpiresIn=3600,
+  )
+
+  return Ok({
+    "upload_url": presigned_url,
+    "image_upload_id": image_upload.id,
+  })
