323b5527b083faa7c126108f8b32fb9b8ed26807
thomasjthomasj <thomas.marchant@justabout.com>
2026-08-26 11:12:41 +0100

    just use a normal listing

--- apps/frontend/app/artist/[slug]/page.tsx
+++ apps/frontend/app/artist/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from "@/_components/PageLayout";
 import { ReleaseArticleLink } from "@/_components/ReleaseArticleLink";
+import { Listing as ReviewListing } from "@/_components/ReviewArchive/Listing";
 import { ReviewGrid } from "@/_components/ReviewGrid";
 import { handleError } from "@/_fns/handle-error";
 import { get } from "@/_utils/api.server";
@@ -57,46 +58,12 @@ const Artist = async ({ params }: ArtistProps) => {
   const { data: artist } = artistResponse;
   const { data: articles } = articlesResponse;
 
-  const top = artist.featured_review_id
-    ? articles.filter((a) => a.id === artist.featured_review_id).shift()
-    : null;
-  const filteredArticles = top
-    ? articles.filter((a) => a.id !== top.id)
-    : articles;
-
   return (
     <PageLayout title={artist.name}>
       <div className="w-full flex flex-col gap-[25px] pt-[10px]">
-        {top?.release && (
-          <>
-            <div className="hidden sm:flex flex-col border border-tertiary-500 p-[10px]">
-              <ReleaseArticleLink article={top} size="lg" showReviewType />
-            </div>
-            <div className="block sm:hidden p-[10px] border border-tertiary-500">
-              <a href={`/article/${top.id}/${top.slug}`}>
-                <div className="flex flex-col items-center gap-[15px]">
-                  <img
-                    className="border border-background-500 w-full aspect-square"
-                    src={top.release.images.md.url}
-                    alt={`${top.release.title} cover art`}
-                    height={300}
-                    width={300}
-                  />
-                  <div className="flex flex-col items-center">
-                    <h3>{top.release.title}</h3>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </>
-        )}
-        {!!filteredArticles.length && (
+        {!!articles.length && (
           <div>
-            <ReviewGrid
-              articles={filteredArticles}
-              className="lg:grid-cols-4"
-              textClassName="text-[14px] font-bold"
-            />
+            <ReviewListing articles={articles} />
           </div>
         )}
       </div>
