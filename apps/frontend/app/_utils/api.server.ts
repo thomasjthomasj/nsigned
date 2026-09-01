cbb3bfd44348072b7b962216c2c258abda35e00e
thomasjthomasj <thomas.marchant@justabout.com>
2026-09-01 15:45:33 +0100

    api post server function

--- apps/frontend/app/_utils/api.server.ts
+++ apps/frontend/app/_utils/api.server.ts
@@ -11,14 +11,56 @@ import type {
   ErrorStatus,
   LoggedInUser,
   QueryParams,
+  Json,
   Response,
 } from "@/_types/api";
+
+type CookieOptions =
+  | {
+      withAuth: true;
+      withCookies?: true;
+    }
+  | {
+      withAuth?: false;
+      withCookies?: boolean;
+    };
+
+type PostParams = {
+  endpoint: string;
+  data?: Json;
+  withAuth?: boolean;
+};
 
 type GetParams = {
   endpoint: string;
   data?: QueryParams;
   withAuth?: boolean;
   cacheKey?: string;
+};
+
+const request = async <TJson>(
+  makeRequest: (cookie: string) => Promise<Response<TJson>>,
+  withAuth: boolean,
+  cookie: string = "",
+): Promise<Response<TJson>> => {
+  const response = await makeRequest(cookie);
+  if (withAuth && response.status === 401) {
+    const refresh = await fetch(getEndpoint("users/refresh"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: cookie,
+      },
+      cache: "no-store",
+    });
+    if (refresh.ok) {
+      const newCookie = refresh.headers.get("set-cookie");
+      if (newCookie) {
+        return makeRequest(newCookie);
+      }
+    }
+  }
+  return response;
 };
 
 export const get = async <TJson = {}>({
@@ -71,25 +113,7 @@ export const get = async <TJson = {}>({
     };
   };
 
-  const response = await makeRequest(cookieHeader);
-  if (withAuth && response.status === 401) {
-    const refresh = await fetch(getEndpoint("users/refresh"), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: cookieHeader,
-      },
-      cache: "no-store",
-    });
-    if (refresh.ok) {
-      const newCookie = refresh.headers.get("set-cookie");
-      if (newCookie) {
-        return makeRequest(newCookie);
-      }
-    }
-  }
-
-  return response;
+  return request<TJson>(makeRequest, withAuth, cookieHeader);
 };
 
 export const getMe = async () =>
@@ -97,3 +121,40 @@ export const getMe = async () =>
     endpoint: "users/me",
     withAuth: true,
   });
+
+export const post = async <TJson = {}>({
+  endpoint,
+  data,
+  withAuth = true,
+}: PostParams) => {
+  const cookieHeader = (await cookies()).toString();
+  const makeRequest = async (cookie: string): Promise<Response<TJson>> => {
+    const result = await fetch(getEndpoint(endpoint), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(withAuth ? { Cookie: cookie } : {}),
+      },
+      body: JSON.stringify(data ?? {}),
+    });
+
+    const resultData = await result.json();
+    if (result.ok) {
+      return {
+        ok: true,
+        status: result.status,
+        data: resultData as TJson,
+        cached: false,
+      };
+    }
+
+    return {
+      ok: false,
+      status: result.status as ErrorStatus,
+      data: resultData as Error,
+      cached: false,
+    };
+  };
+
+  return request<TJson>(makeRequest, withAuth, cookieHeader);
+};
